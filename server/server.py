1b36e440661d9a1558fe4c1bb95465b967b9c280
Starl1t <93038650+Starl1t@users.noreply.github.com>
2025-03-24 15:29:21 +0300

    PADGE-3: Added subprocess to run calculator from server

--- server/server.py
+++ server/server.py
@@ -1,13 +1,33 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import subprocess
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # Handling POST requests
+
+        use_float = False
+
+        expression = "12+3"
+
+        args = ["../build/app.exe"]
+        if use_float:
+            args.append("--float")
+
+        res = subprocess.run(
+            args,
+            input=expression,
+            text=True,
+            capture_output=True
+        )
+
+        ans = res.stdout
+        print(ans)
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello World")
+        self.wfile.write(ans.encode())
 
 
 # Starting server on port 8000
